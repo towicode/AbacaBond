7dda3f2b812fb9d79229835043871f12e1bfc20d
Todd Wickizer <toddwickizer@gmail.com>
2017-04-24 23:01:13 -0700

    abc

--- ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/Source.cpp
@@ -14,27 +14,116 @@
 
 #include <boost/asio.hpp>
 #include <boost/array.hpp>
+#include <Windows.h>
 
 using boost::asio::ip::tcp;
+namespace pt = boost::posix_time;
 
 unsigned short const PORT = 19876;
 
 jclass Client;
 jclass Character;
+jclass Canvas;
+jclass MouseEvent;
+jclass MouseListener;
+
 
 char* ClientObf = "Lav;";
 char* CharacterObf = "Lt;";
+char* CanvasObf = "Ljava/awt/Canvas;";
+char* MouseListenerObf = "Ljava/awt/event/MouseListener;";
+char* MouseEventObf = "Ljava/awt/event/MouseEvent;";
+char* ComponentObf = "Ljava/awt/Component;";
+
+
+
 
 //	used to access localPlayer
 char* localCharField = "hl";
+char* localCanvasField = "qc";
 
 //	Player fields
 char * player_name = "f";
 
+static std::string moveMouse(jvmtiEnv *jvmti, JNIEnv *env) {
+
+	jfieldID canvas_field = env->GetStaticFieldID(Client, localCanvasField, CanvasObf);
+	if (!canvas_field) {
+		std::string messages = "ERROR no canvas field\n";
+		return messages;
+	}
+
+	jobject local_canvas = env->GetStaticObjectField(Client, canvas_field);
+	if (!local_canvas) {
+		std::string message = "ERROR couldn't grab local canvas\n";
+		return message;
+	}
+
+	jmethodID constructor;
+	constructor = env->GetMethodID(MouseEvent, "<init>", "(Ljava/awt/Component;IJIIIIZ)V");
+	if (!constructor) {
+		std::string message = "ERROR couldn't grab constructor for mouse event\n";
+		return message;
+	}
+
+	jboolean fal = false;
+	jint button = 1;
+	jint zero = 0;
+	pt::ptime time_t_epoch(boost::gregorian::date(1970, 1, 1));
+	pt::ptime now = pt::microsec_clock::local_time();
+	pt::time_duration diff = now - time_t_epoch;
+	long x = diff.total_milliseconds();
+	jlong when = x;
+
+	jmethodID getMouseListeners = env->GetMethodID(Canvas, "getMouseListeners", "()[Ljava/awt/event/MouseListener;");
+	if (!getMouseListeners) {
+		std::string message = "ERROR couldn't get Mouse Listener method\n";
+		return message;
+	}
+
+	jobjectArray mouse_array = (jobjectArray) env->CallObjectMethod(local_canvas, getMouseListeners);
+	if (!mouse_array) {
+		std::string message = "ERROR Get array of mouse listeners\n";
+		return message;
+	}
+
+	jobject mouse_proper = env->GetObjectArrayElement(mouse_array, 0);
+	if (!mouse_proper) {
+		std::string message = "ERROR Get first object of mouse array.\n";
+		return message;
+	}
+
+	jmethodID create_event = env->GetMethodID(MouseListener, "mousePressed", "(Ljava/awt/event/MouseEvent;)V");
+	if (!create_event) {
+		std::string message = "ERROR Get first object of mouse array.\n";
+		return message;
+	}
+	for (int i = 0; i < 200; i++) {
+		jobject my_event;
+		jint x = i;
+		jint y = i;
+
+		printf("CREATING MOUSE EVENT AT %d,%d\n", i, i);
+
+		my_event = env->NewObject(MouseEvent, constructor, local_canvas, button, when, zero, x, y, button, fal);
+		if (!my_event) {
+			std::string message = "ERROR couldn't create the Mouse Event.\n";
+			return message;
+		}
+
+		Sleep(200);
+
+		env->CallVoidMethod(mouse_proper, create_event, my_event);
+
+	}
+
+	return "True";
+}
+
 
 static std::string getMyRSPlayer(jvmtiEnv *jvmti, JNIEnv *env) {
 
-	jfieldID character_field = env->GetStaticFieldID(Client, localCharField, ClientObf);
+	jfieldID character_field = env->GetStaticFieldID(Client, localCharField, CharacterObf);
 	if (!character_field) {
 		std::string messages = "ERROR no localPlayer field\n";
 		return messages;
@@ -70,6 +159,9 @@ static std::string lookup(int command, jvmtiEnv *jvmti, JNIEnv *env) {
 	switch (command) {
 	case 1:
 		return getMyRSPlayer(jvmti, env);
+
+	case 2:
+		return moveMouse(jvmti, env);
 	}
 
 }
@@ -131,7 +223,7 @@ Agent_OnLoad(JavaVM *vm, char *options, void *reserved)
 		char *generic;
 		err = jvmti->GetClassSignature(cls, &signature, &generic);
 		if (err == JVMTI_ERROR_NONE) {
-			printf("THE dog: %s \n", signature);
+			//printf("THE dog: %s \n", signature);
 			// Just be simple and look for our classes...
 			if (strstr(signature, ClientObf) != NULL) {
 				printf("Found Client Class\n");
@@ -142,6 +234,22 @@ Agent_OnLoad(JavaVM *vm, char *options, void *reserved)
 				printf("Found Character Class\n");
 				Character = cls;
 			}
+
+			if (strstr(signature, CanvasObf) != NULL) {
+				printf("Found Canvas Class\n");
+				Canvas = cls;
+			}
+
+			if (strstr(signature, MouseEventObf) != NULL) {
+				printf("Found MouseEvent Class\n");
+				MouseEvent = cls;
+			}
+
+			if (strstr(signature, MouseListenerObf) != NULL) {
+				printf("Found MouseListener Class\n");
+				MouseListener = cls;
+			}
+			
 		}
 		else {
 			fprintf(stderr, "Couldn't check class :(\n");
@@ -172,7 +280,7 @@ Agent_OnLoad(JavaVM *vm, char *options, void *reserved)
 			//std::string message = "Server string to send to client\n";
 			//boost::asio::write(socket, boost::asio::buffer(message));
 			//lookup(1, jvmti, jni);
-			std::string message = lookup(1, jvmti, jni);
+			std::string message = lookup(2, jvmti, jni);
 			boost::asio::write(socket, boost::asio::buffer(message));
 
 		}
